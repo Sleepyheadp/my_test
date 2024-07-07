ad2f614c253a4ee80f8f4f00de5918f1b91c8ad8
Sleepyheadp <sleepyheadp322@gmail.com>
2024-07-07 12:04:38 +0800

    条件渲染逻辑判断(v-if/v-show)

--- vue_test/src/components/Foo.vue
+++ vue_test/src/components/Foo.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
-    <div>Foo <Bar></Bar></div>
+    <div>
+      <!-- Foo -->
+      <!-- <Bar></Bar> -->
+      <a id="profile" href="">My Profile</a>
+      <a href="" v-if="admin" id="admin">Admin</a>
+      <ul href="" v-show="shouldShowDropdown" id="user-dropdown"></ul>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script>
 import Bar from './Bar.vue'
+export default {
+  components: {
+    Bar
+  },
+  data() {
+    return {
+      admin: false,
+      shouldShowDropdown: false
+    }
+  }
+}
 </script>
 <style scoped></style>
--- vue_test/src/components/__tests__/Foo.spec.js
+++ vue_test/src/components/__tests__/Foo.spec.js
@@ -4,17 +4,50 @@ import { mount, shallowMount } from '@vue/test-utils'
 import Foo from '../Foo.vue'
 import { describe } from 'vitest'
 
-describe('Foo.vue', () => {
+/* describe('Foo.vue', () => {
   it('mount', () => {
     const wrapper = mount(Foo)
     console.log(wrapper.text())
     // expect(wrapper.text()).toBe('Foo')
   })
-})
-describe('Foo.vue', () => {
+}) */
+/* describe('Foo.vue', () => {
   it('shallowMount', () => {
     const wrapper = shallowMount(Foo)
     console.log(wrapper.text())
     // expect(wrapper.text()).toBe('Foo')
+  })
+}) */
+
+describe('Foo.vue', () => {
+  /* get:用于获取一定存在的元素 */
+  it('get element a', () => {
+    const wrapper = mount(Foo)
+    // get方法获取的元素必须是一定存在的
+    const profile = wrapper.get('#profile')
+    expect(profile.text()).toBe('My Profile')
+  })
+  /* find:用于查找元素是否存在 */
+  it('(v-if)Determine if an element exists - false', () => {
+    const wrapper = mount(Foo)
+    const admin = wrapper.find('#admin')
+    expect(admin.exists()).toBe(false) // 默认是不存在的,admin=false
+  })
+  // find方法的第二个参数是一个对象,可以传入一个data对象,用于设置data属性
+  it('(v-if)Determine if an element exists - true', () => {
+    const wrapper = mount(Foo, {
+      data() {
+        return {
+          admin: true
+        }
+      }
+    })
+    const userDropdown = wrapper.find('#user-dropdown')
+    expect(userDropdown.exists()).toBe(true)
+  })
+  it('(v-show)Determines if an element is visible - false', () => {
+    const wrapper = mount(Foo)
+    const userDropdown = wrapper.find('#user-dropdown')
+    expect(userDropdown.isVisible()).toBe(false)
   })
 })
